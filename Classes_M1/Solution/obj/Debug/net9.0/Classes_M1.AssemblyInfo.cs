4f266b02e6dbe502a0cbc56779d1c7c53374f612
slothgina <gina.slothstudio@zohomail.com>
2026-05-12 02:33:50 -0400

    Split the BankCustomer class into two partial class files

--- Classes_M1/Solution/obj/Debug/net9.0/Classes_M1.AssemblyInfo.cs
+++ Classes_M1/Solution/obj/Debug/net9.0/Classes_M1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes_M1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+859cbb6cf27c158447fc5e5f554bc47cbbc1705f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes_M1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes_M1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
